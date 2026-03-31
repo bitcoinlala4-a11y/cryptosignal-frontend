d366477bf6d204d7044db973267b8539054e9230
bitcoinlala4-a11y <bitcoinlala4@gmail.com>
2026-03-31 12:57:16 +0200

    Responsive mobile all pages

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="fr">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="theme-color" content="#0f0f1a" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Head>
       <body>
         <Main />
         <NextScript />
